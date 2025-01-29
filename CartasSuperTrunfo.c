08d1732160703b96efa57e0fc5d81ea09414362e
Andre Alexandre Oliveira <74605067+dreoliveira19@users.noreply.github.com>
2025-01-29 12:10:34 +0000

    Versão Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,7 +10,27 @@ typedef struct {
     float area;
     float pib;
     int pontosTuristicos;
+    float densidadePopulacional; 
+    float pibPerCapita;          
 } Carta;
+
+// Função para calcular Densidade Demográfica da cidade
+void calcularDensidade(Carta *c) {
+    if (c->area > 0) {
+        c->densidadePopulacional = c->populacao / c->area;
+    } else {
+        c->densidadePopulacional = 0;
+    }
+}
+
+// Função para calcular PIB per Capta da cidade
+void calcularPib(Carta *c) {
+    if (c->populacao > 0) {
+        c->pibPerCapita = c->pib * 1000000000 / c->populacao; // PIB em reais por pessoa
+    } else {
+        c->pibPerCapita = 0;
+    }
+}
 
 // Função para exibir os dados de uma carta
 void exibirCarta(Carta c) {
@@ -20,7 +40,9 @@ void exibirCarta(Carta c) {
     printf("Nome da Cidade: %s\n", c.nomeCidade);
     printf("População: %d\n", c.populacao);
     printf("Área: %.2f km²\n", c.area);
+    printf("Densidade Populacional: %.2f pessoas/km²\n", c.densidadePopulacional);
     printf("PIB: %.2f bilhões de reais\n", c.pib);
+    printf("PIB per Capita: %.2f reais\n", c.pibPerCapita);
     printf("Número de Pontos Turísticos: %d\n", c.pontosTuristicos);
     printf("----------------------\n");
 }
@@ -30,7 +52,6 @@ int main() {
     int totalCartas = 0;
     int opcao = 0;
     char opcaoContinuar = 'S';
-
     printf("Bem-vindo ao sistema Super Trunfo de Países!\n\n");
 
     while(opcao != 9){ // Menu de Opções
@@ -73,6 +94,10 @@ int main() {
                 printf("Digite o número de pontos turísticos: ");
                 scanf(" %d", &novaCarta.pontosTuristicos);
 
+                // Calcular Densidade e PIB Per Capta
+                calcularDensidade(&novaCarta);
+                calcularPib(&novaCarta);
+
                 // Armazenar a carta no array
                 cartas[totalCartas] = novaCarta;
                 totalCartas++;
@@ -103,7 +128,6 @@ int main() {
             printf("\nTotal de cartas cadastradas: %d\n", totalCartas);
         }
 
-        
     }
     return 0;
 }
